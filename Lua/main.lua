21347970036d866e6319a0ce825ba3e0be21c552
liubo <544079846@qq.com>
2024-06-21 19:13:49 +0800

    随便写一点

---
+++ Lua/main.lua
@@ -0,0 +1,20 @@
+package.path = package.path..";?.lua.txt"
+
+require "battle.init"
+
+local classA = battle.class("a")
+function classA:Ctor()
+    print("classA:Ctor")
+end
+
+function classA:hello()
+    print("classA:hello")
+end
+
+local classB = battle.class("b", classA)
+function classB:Ctor()
+    print("classB:Ctor")
+end
+
+local inst = classB()
+inst:hello()
